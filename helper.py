3ba4c2c4fbce2948bc709a70fe892c87bcefe7cd
ikia <shivi3015v@gmail.com>
2026-04-26 16:48:21 +0530

    Update helper.py
    most common word return

--- helper.py
+++ helper.py
@@ -87,7 +87,7 @@ def most_common_words(selected_user,df):
             if word not in stop_words:
                 words.append(word)
 
-    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    most_common_df = pd.DataFrame(most_common_words, columns=["word", "count"])
     return most_common_df
 
 def emoji_helper(selected_user,df):
